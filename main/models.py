6ccfe80ae15807696784a4e7fdb221a59cf2e61b
FakhriHusainiRomza23 <fakhri.husaini41@ui.ac.id>
2025-09-09 17:09:22 +0700

    Tugas 2: Django MVT implementation

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
 
-class News(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = [
         ('transfer', 'Transfer'),
         ('update', 'Update'),
@@ -11,22 +11,20 @@ class News(models.Model):
         ('analysis', 'Analysis'),
     ]
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
         return self.title
     
     @property
-    def is_news_hot(self):
-        return self.news_views > 20
+    def is_product_hot(self):
+        return self.product_views > 20
         
     def increment_views(self):
-        self.news_views += 1
+        self.product_views += 1
         self.save()
